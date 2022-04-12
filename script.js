b10d51fa6b2070127085f52a7504967781a8f7ee
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-12 14:38:38 +0100

    Exercise 2 done

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 
+
 let usersFunction = async () => {
 
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const parsedBody = await response.json();
     console.log(parsedBody)
 
-    let row1 = document.querySelector(".row1");
+    
+    array1 = [...parsedBody]
 
+    let row1 = document.querySelector(".row1");
+    
 
     parsedBody.forEach(name => {
         let col2 = document.createElement("div")
@@ -17,44 +21,130 @@ let usersFunction = async () => {
         <p>${name.name}</p>
         `
         row1.appendChild(col2)
-
-
-
-
-
     })
-
 }
 
 let selectedValue = []
 
 let selectorValue = (event) => {
-let selector = document.getElementById("names")
-console.log(event.target.value)
-selectedValue.push(event.target.value)
+    console.log(event.target.value)
+     selectedValue.push(event.target.value)    
+
+   if(event.target.value === "email") {
+    return showEmailFunction() 
+    } else if(event.target.value === "username") {
+        return showUsernameFunction()
+    } else if (event.target.value === "name") {
+        return showNameFunction()
+    }
+}
 
 
-let showOptionFunction = async (event) => {
+
+/* THIS IS TEH EMAILK FUNCTIOIN */
+let showEmailFunction = async (event) => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const parsedBody = await response.json();
     console.log(parsedBody)
-    /* console.log(event) */
+    
+
     let row1 = document.querySelector(".row1");
 
-    if(selectedValue === email) {
-        parsedBody.forEach(name => {
+        row1.innerHTML = ""
+
+        parsedBody.forEach(email => {
+        let col2 = document.createElement("div")
+        col2.classList.add("col-12") 
+        col2.innerHTML = `<p>${email.email}
+        `
+        row1.appendChild(col2)
+    })
+    }
+    /* THISIS THE USERNAME FUNCTIOIN */
+    let showUsernameFunction = async (event) => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const parsedBody = await response.json();
+        console.log(parsedBody)
+        let row1 = document.querySelector(".row1");
+    
+            row1.innerHTML = ""
+    
+            parsedBody.forEach(username => {
+            let col2 = document.createElement("div")
+            col2.classList.add("col-12") 
+            col2.innerHTML = `<p>${username.username}
+            `
+            row1.appendChild(col2)
+        })
+        }
+    
+            /* tHIS IS THE NAME FUNCTION */
+        let showNameFunction = async (event) => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users")
+            const parsedBody = await response.json();
+            console.log(parsedBody)
+            
+        
+            let row1 = document.querySelector(".row1");
+        
+                row1.innerHTML = ""
+        
+                parsedBody.forEach(name => {
+                let col2 = document.createElement("div")
+                col2.classList.add("col-12") 
+                col2.innerHTML = `<p>${name.name}
+                `
+                row1.appendChild(col2)
+            })
+            }
+
+
+
+
+/* BUTTON INPUT SERACH FUNCTION */
+
+let searchButton = (event) =>  {
+    console.log(event.target.parentElement.querySelector("input").value)
+
+        console.log(array1)
+
+        let searchQuery = event.target.parentElement.querySelector("input").value;
+
+        let row1 = document.querySelector(".row1");
+        row1.innerHTML = ""
+
+/*      console.log(array1.filter(array => array.name.toLowerCase().includes(searchQuery.toLowerCase())))
+ */
+     array1.filter(array => array.name.toLowerCase().includes(event.target.parentElement.querySelector("input").value.toLowerCase()))
+     .forEach(name => {
+        let col2 = document.createElement("div")
+        col2.classList.add("col-12") 
+        col2.innerHTML = `<p>${name.name}
+        `
+        row1.appendChild(col2)
+     })
+
+     array1.filter(array => array.username.toLowerCase().includes(event.target.parentElement.querySelector("input").value.toLowerCase()))
+     .forEach(name => {
+        let col2 = document.createElement("div")
+        col2.classList.add("col-12") 
+        col2.innerHTML = `<p>${name.username}
+        `
+        row1.appendChild(col2)
+     })
+
+     array1.filter(array => array.email.toLowerCase().includes(event.target.parentElement.querySelector("input").value.toLowerCase()))
+     .forEach(name => {
         let col2 = document.createElement("div")
         col2.classList.add("col-12") 
         col2.innerHTML = `<p>${name.email}
         `
         row1.appendChild(col2)
-    })
-    }
-
+     })
+        
 }
-showOptionFunction();
+            
 
-}
 
 
 
@@ -62,4 +152,5 @@ showOptionFunction();
 
 window.onload = () => {
     usersFunction();
+
 }
